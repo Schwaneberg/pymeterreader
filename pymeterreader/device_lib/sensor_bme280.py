d4c73707f8eb893337d14561b1b7e99b5a153f0b
Oliver Schwaneberg <oliver.schwaneberg@gmail.com>
2020-10-21 22:19:11 +0200

    Refactored BME280 implementation using construct library. Added a unit test.

--- pymeterreader/device_lib/sensor_bme280.py
+++ pymeterreader/device_lib/sensor_bme280.py
@@ -4,14 +4,38 @@ Based on Matt Hawkins's implementation on
 https://www.raspberrypi-spy.co.uk/
 Created 2020.10.17 by Oliver Schwaneberg
 """
+import time
 from logging import debug, error, warning
 import typing as tp
-import smbus
-import time
-from ctypes import c_short
 from threading import Lock
+import smbus
+from construct import Struct, BytesInteger, BitStruct, BitsInteger
 from pymeterreader.device_lib.base import BaseReader
 from pymeterreader.device_lib.common import Sample
+
+AUX_STRUCT = BitStruct('H4' / BitsInteger(12, True), 'H5' / BitsInteger(12, True))
+COMP_PARAM_STRUCT = Struct('T1' / BytesInteger(2, False, True),
+                           'T2' / BytesInteger(2, True, True),
+                           'T3' / BytesInteger(2, True, True),
+                           'P1' / BytesInteger(2, False, True),
+                           'P2' / BytesInteger(2, True, True),
+                           'P3' / BytesInteger(2, True, True),
+                           'P4' / BytesInteger(2, True, True),
+                           'P5' / BytesInteger(2, True, True),
+                           'P6' / BytesInteger(2, True, True),
+                           'P7' / BytesInteger(2, True, True),
+                           'P8' / BytesInteger(2, True, True),
+                           'P9' / BytesInteger(2, True, True),
+                           'H1' / BytesInteger(1, False),
+                           'H2' / BytesInteger(2, True, True),
+                           'H3' / BytesInteger(1, False),
+                           'bitfield' / BitStruct('n1' / BitsInteger(4),
+                                                  'n2' / BitsInteger(4),
+                                                  'n3' / BitsInteger(4),
+                                                  'n4' / BitsInteger(4),
+                                                  'n5' / BitsInteger(4),
+                                                  'n6' / BitsInteger(4)),
+                           'H6' / BytesInteger(1, False))
 
 
 class Bme280Reader(BaseReader):
@@ -34,6 +58,8 @@ class Bme280Reader(BaseReader):
         :param initial_baudrate: Baudrate used to send the request
         :param baudrate: Baudrate used to read the answer
         """
+        if kwargs:
+            warning(f"Bme280Reader: Unknown parameter{'s' if len(kwargs) > 1 else ''}: {', '.join(kwargs.keys())}")
         if isinstance(meter_id, str):
             if meter_id.lower().startswith('0x'):
                 meter_id = int(meter_id.lower(), 16)
@@ -57,7 +83,6 @@ class Bme280Reader(BaseReader):
         :return: True, if successful
         """
         # pylint: disable=too-many-locals, too-many-statements
-        # TODO Refactoring required
         sample = Sample()
         with self.__I2C_LOCK:
             try:
@@ -86,49 +111,26 @@ class Bme280Reader(BaseReader):
                 cal2 = bus.read_i2c_block_data(self.meter_id, 0xA1, 1)
                 cal3 = bus.read_i2c_block_data(self.meter_id, 0xE1, 7)
 
-                # Convert byte data to word values
-                dig_t1 = self.__to_uin16(cal1, 0)
-                dig_t2 = self.__to_int16(cal1, 2)
-                dig_t3 = self.__to_int16(cal1, 4)
-
-                dig_p1 = self.__to_uin16(cal1, 6)
-                dig_p2 = self.__to_int16(cal1, 8)
-                dig_p3 = self.__to_int16(cal1, 10)
-                dig_p4 = self.__to_int16(cal1, 12)
-                dig_p5 = self.__to_int16(cal1, 14)
-                dig_p6 = self.__to_int16(cal1, 16)
-                dig_p7 = self.__to_int16(cal1, 18)
-                dig_p8 = self.__to_int16(cal1, 20)
-                dig_p9 = self.__to_int16(cal1, 22)
-
-                dig_h1 = self.__to_uint8(cal2, 0)
-                dig_h2 = self.__to_int16(cal3, 0)
-                dig_h3 = self.__to_uint8(cal3, 2)
-
-                dig_h4 = self.__to_int8(cal3, 3)
-                dig_h4 = (dig_h4 << 24) >> 20
-                dig_h4 = dig_h4 | (self.__to_int8(cal3, 4) & 0x0F)
-
-                dig_h5 = self.__to_int8(cal3, 5)
-                dig_h5 = (dig_h5 << 24) >> 20
-                dig_h5 = dig_h5 | (self.__to_uint8(cal3, 4) >> 4 & 0x0F)
-
-                dig_h6 = self.__to_int8(cal3, 6)
+                dig = COMP_PARAM_STRUCT.parse(bytes(cal1) + bytes(cal2) + bytes(cal3))
+                dig2 = AUX_STRUCT.parse(bytes([dig.bitfield.n1 << 4 | dig.bitfield.n2,
+                                               dig.bitfield.n4 << 4 | dig.bitfield.n5,
+                                               dig.bitfield.n6 << 4 | dig.bitfield.n3]))
 
                 # Wait in ms (Datasheet Appendix B: Measurement time and current calculation)
-                wait_time = 1.25 + (2.3 * oversample_temp) + ((2.3 * oversample_pres) + 0.575) + (
-                        (2.3 * oversample_hum) + 0.575)
+                wait_time = 1.25 + 2.3 * oversample_temp + 2.3 * oversample_pres + 0.575 \
+                            + 2.3 * oversample_hum + 0.575
                 time.sleep(wait_time / 1000)  # Wait the required time
 
                 # Read temperature/pressure/humidity
                 data = bus.read_i2c_block_data(self.meter_id, reg_data, 8)
+                bus.close()
                 pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
                 temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
                 hum_raw = (data[6] << 8) | data[7]
 
                 # Refine temperature
-                var1 = (((temp_raw >> 3) - (dig_t1 << 1)) * (dig_t2)) >> 11
-                var2 = (((((temp_raw >> 4) - (dig_t1)) * ((temp_raw >> 4) - (dig_t1))) >> 12) * (dig_t3)) >> 14
+                var1 = (((temp_raw >> 3) - (dig['T1'] << 1)) * dig['T2']) >> 11
+                var2 = (((((temp_raw >> 4) - dig['T1']) * ((temp_raw >> 4) - dig['T1'])) >> 12) * dig['T3']) >> 14
                 t_fine = var1 + var2
                 temperature = float(((t_fine * 5) + 128) >> 8)
                 sample.channels.append({'objName': 'TEMPERATURE',
@@ -137,29 +139,29 @@ class Bme280Reader(BaseReader):
 
                 # Refine pressure and adjust for temperature
                 var1 = t_fine / 2.0 - 64000.0
-                var2 = var1 * var1 * dig_p6 / 32768.0
-                var2 = var2 + var1 * dig_p5 * 2.0
-                var2 = var2 / 4.0 + dig_p4 * 65536.0
-                var1 = (dig_p3 * var1 * var1 / 524288.0 + dig_p2 * var1) / 524288.0
-                var1 = (1.0 + var1 / 32768.0) * dig_p1
+                var2 = var1 * var1 * dig['P6'] / 32768.0
+                var2 = var2 + var1 * dig['P5'] * 2.0
+                var2 = var2 / 4.0 + dig['P4'] * 65536.0
+                var1 = (dig['P3'] * var1 * var1 / 524288.0 + dig['P2'] * var1) / 524288.0
+                var1 = (1.0 + var1 / 32768.0) * dig['P1']
                 if var1 == 0:
                     pressure = 0
                 else:
                     pressure = 1048576.0 - pres_raw
                     pressure = ((pressure - var2 / 4096.0) * 6250.0) / var1
-                    var1 = dig_p9 * pressure * pressure / 2147483648.0
-                    var2 = pressure * dig_p8 / 32768.0
-                    pressure = pressure + (var1 + var2 + dig_p7) / 16.0
+                    var1 = dig['P9'] * pressure * pressure / 2147483648.0
+                    var2 = pressure * dig['P8'] / 32768.0
+                    pressure = pressure + (var1 + var2 + dig['P7']) / 16.0
                 sample.channels.append({'objName': 'PRESSURE',
                                         'value': pressure / 100.0,
                                         'unit': 'hPa'})
 
                 # Refine humidity
                 humidity = t_fine - 76800.0
-                humidity = (hum_raw - (dig_h4 * 64.0 + dig_h5 / 16384.0 * humidity)) * (
-                        dig_h2 / 65536.0 * (1.0 + dig_h6 / 67108864.0 * humidity
-                                            * (1.0 + dig_h3 / 67108864.0 * humidity)))
-                humidity = humidity * (1.0 - dig_h1 * humidity / 524288.0)
+                humidity = ((hum_raw - (dig2['H4'] * 64.0 + dig2['H5'] / 16384.0 * humidity))
+                            * (dig['H2'] / 65536.0 * (1.0 + dig['H6'] / 67108864.0 * humidity
+                                                      * (1.0 + dig['H3'] / 67108864.0 * humidity))))
+                humidity = humidity * (1.0 - dig['H1'] * humidity / 524288.0)
                 if humidity > 100:
                     humidity = 100
                 elif humidity < 0:
@@ -173,40 +175,6 @@ class Bme280Reader(BaseReader):
                 else:
                     error(f"Bme280Reader: Cannot detect BME280 at add {self.meter_id:02x}")
         return sample
-
-    @staticmethod
-    def __to_int16(data: bytes, index: int) -> int:
-        """
-        return two bytes from data as a signed 16-bit value
-        :param data:
-        :param index:
-        :return:
-        """
-        return c_short((data[index + 1] << 8) + data[index]).value
-
-    @staticmethod
-    def __to_uin16(data: bytes, index: int) -> int:
-        """
-        return two bytes from data as an unsigned 16-bit value
-        :param data:
-        :param index:
-        :return:
-        """
-        return (data[index + 1] << 8) + data[index]
-
-    @staticmethod
-    def __to_int8(data: bytes, index: int) -> int:
-        # return one byte from data as a signed char
-        result = data[index]
-        if result > 127:
-            result -= 256
-        return result
-
-    @staticmethod
-    def __to_uint8(data: bytes, index: int) -> int:
-        # return one byte from data as an unsigned char
-        result = data[index] & 0xFF
-        return result
 
     def read_chip_info(self) -> tp.Optional[tp.Tuple[int, int]]:
         """
--- pymeterreader/device_lib/test_sensor_bme280.py
+++ pymeterreader/device_lib/test_sensor_bme280.py
@@ -1,11 +1,55 @@
 import unittest
+from unittest import mock
 from pymeterreader.device_lib.sensor_bme280 import Bme280Reader
+
+pres_raw = 343552
+temp_raw = 516760
+humi_raw = 24840
+
+class mock_bus:
+    cal1 = [109, 111, 157, 104, 50, 0, 33, 142, 248, 214, 208, 11,
+            201, 28, 230, 255, 249, 255, 172, 38, 10, 216, 189, 16]
+    cal2 = [75]
+    cal3 = [135, 1, 0, 15, 46, 3, 30]
+    data = [83, 224, 0, 126, 41, 128, 97, 8]
+
+    def __init__(self):
+        self.written = []
+        self.closed = False
+
+    def write_byte_data(self, addr, reg, val):
+        assert not self.closed
+        self.written.append((addr, reg, val))
+
+    def read_i2c_block_data(self, addr, reg, length):
+        assert addr == 0x76
+        assert not self.closed
+        if reg == 0x88 and length == 24:
+            return self.cal1
+        elif reg == 0xA1 and length == 1:
+            return self.cal2
+        elif reg == 0xE1 and length == 7:
+            return self.cal3
+        elif reg == 0xF7 and length == 8:
+            return self.data
+        raise AssertionError("Unknown request")
+
+    def close(self):
+        self.closed = True
+
+
+ref_channels = [{'objName': 'TEMPERATURE', 'value': 19.27, 'unit': '°C'},
+                {'objName': 'PRESSURE', 'value': 998.5556593146621, 'unit': 'hPa'},
+                {'objName': 'HUMIDITY', 'value': 50.93572133159069, 'unit': '%'}]
 
 
 class TestBme280(unittest.TestCase):
-    def test_something(self):
+    @mock.patch('pymeterreader.device_lib.sensor_bme280.smbus', autospec=True)
+    def test_readout(self, mock_smbus):
+        mock_smbus.SMBus.return_value = mock_bus()
         reader = Bme280Reader("0x76")
-
+        sample = reader.poll()
+        self.assertEqual(ref_channels, sample.channels)
 
 
 if __name__ == '__main__':
