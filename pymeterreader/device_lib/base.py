928b0c67d3ea8afe979db644beda2ba19418446c
Norman Gehrsitz <gehrsitz.norman@student.dhbw-kahlsruhe.de>
2020-12-22 19:45:23 +0100

    Split Serial access Components from BaseReader into SerialReader This allows for code sharing between all protocols that require a serial connection. By constructing the serial connection using serial_for_url() Cross Plattform support and RFC2217 are possible. Configurations using the old regex support need to be adapted. Regex support is now present in the form of the hwgrep url format:  https://pyserial.readthedocs.io/en/latest/url_handlers.html#urls  However instead of relying on the regex support creating a static configuration with the wizard is recommended!

--- pymeterreader/device_lib/base.py
+++ pymeterreader/device_lib/base.py
@@ -10,25 +10,19 @@ from pymeterreader.device_lib.common import Sample, Device
 
 class BaseReader(ABC):
     """
-    Reads meters with SML output via
-    EN 62056-21:2002 compliant optical interfaces.
-    Tested with EMH eHZ electrical meters
-    See https://en.wikipedia.org/wiki/IEC_62056
+    Implementation Base for a Meter Protocol
     """
     PROTOCOL = "ABSTRACT"
     BOUND_INTERFACES = set()
 
     @abstractmethod
-    def __init__(self, meter_id: tp.Union[str, int], tty=r'/dev/ttyUSB\d+', **kwargs):
+    def __init__(self, meter_id: tp.Union[str, int], **kwargs):
         """
         Initialize Meter Reader object
         :param meter_id: meter identification string (e.g. '1 EMH00 12345678')
-        :param tty: Name or regex pattern of the tty node to use
-        :kwargs: device specific parameters
+        :kwargs: implementation specific parameters
         """
         self.meter_id = meter_id
-        self.tty_pattern = tty
-        self._tty_path = None
         if kwargs:
             warning(f'Unknown parameter{"s" if len(kwargs) > 1 else ""}:'
                     f' {", ".join(kwargs.keys())}')
@@ -41,28 +35,6 @@ class BaseReader(ABC):
         :kwargs: parameters for the classes that implement detection
         """
         raise NotImplementedError("This is just an abstract class.")
-
-    def __del__(self):
-        """
-        Set bound interface free
-        """
-        self.tty_path = None
-
-    @property
-    def tty_path(self):
-        return self._tty_path
-
-    @tty_path.setter
-    def tty_path(self, tty_path):
-        if self._tty_path is not None:
-            # Set current interface free
-            self.BOUND_INTERFACES.remove(self._tty_path)
-        if tty_path is not None:
-            # Claim new interface
-            if tty_path in self.BOUND_INTERFACES:
-                raise KeyError(f"{tty_path} already in use.")
-            self.BOUND_INTERFACES.add(tty_path)
-        self._tty_path = tty_path
 
     @abstractmethod
     def poll(self) -> tp.Optional[Sample]:
