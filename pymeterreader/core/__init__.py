eba4f193eb5c42948dd96a50881f8c54c6eff70f
Norman Gehrsitz <gehrsitz.norman@student.dhbw-kahlsruhe.de>
2021-01-04 21:14:29 +0100

    Move MeterReaderTask and MeterReaderNode into core module

---
+++ pymeterreader/core/__init__.py
@@ -0,0 +1,4 @@
+from pymeterreader.core.meter_reader_node import MeterReaderNode
+from pymeterreader.core.meter_reader_task import MeterReaderTask
+
+__all__ = ["MeterReaderNode", "MeterReaderTask"]
